fbc1e1d11615e160b32cc962417a74a9f3150307
kigani <kk.zwak@gmail.com>
2015-12-09 12:48:48 +0100

    add autoprefixer, sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 // Include gulp
 var gulp = require('gulp');
 
-// Include Our Plugins
-var sass = require('gulp-sass');
+// Include plugins
 
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 
 // Compile Our Sass
 gulp.task('sass', function() {
     return gulp.src('assets/styles/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie 10', 'ie 9']
+        }))
+        .pipe(sourcemaps.write('assets/styles/'))
         .pipe(gulp.dest('assets/styles/'));
 });
 
